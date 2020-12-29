39cf42e1ca7c5c4f7bd5657db295f4aa9db94319
Wilson Neely <naruisk.art@gmail.com>
2020-12-28 20:24:24 -0600

    Stage 2 complete, no branch because it was simple

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,10 +1,25 @@
 # Stage 1, used to break the ice and have somewhere to start
+import random
+
+choices = ["scissors", "paper", "rock"]
 ans = input()
-if ans == "scissors":
-    print("Sorry, but the computer chose rock")
-elif ans == "paper":
-    print("Sorry, but the computer chose scissors")
-elif ans == "rock":
-    print("Sorry, but the computer chose paper")
+random.shuffle(choices)
+lose = None
+if ans == choices[0]:
+    lose = -1
+elif ans == "scissors" and choices[0] == "rock":
+    lose = 1
+elif ans == "paper" and choices[0] == "scissors":
+    lose = 1
+elif ans == "rock" and choices[0] == "paper":
+    lose = 1
 else:
-    pass
+    lose = 0
+if lose == 1:
+    print(f"Sorry, but the computer chose {choices[0]}")
+elif lose == 0:
+    print(f"Well done. The computer chose {choices[0]} and failed")
+elif lose == -1:
+    print(f"There is a draw ({choices[0]})")
+else:
+    print("How?")
