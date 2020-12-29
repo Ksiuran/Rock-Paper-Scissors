d1dfa0735a9eee0212d87ea9151a61e669c1e338
Wilson Neely <naruisk.art@gmail.com>
2020-12-28 20:40:41 -0600

    Stage 3 complete, no branch because it was simple

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,25 +1,34 @@
 # Stage 1, used to break the ice and have somewhere to start
 import random
 
-choices = ["scissors", "paper", "rock"]
-ans = input()
-random.shuffle(choices)
-lose = None
-if ans == choices[0]:
-    lose = -1
-elif ans == "scissors" and choices[0] == "rock":
-    lose = 1
-elif ans == "paper" and choices[0] == "scissors":
-    lose = 1
-elif ans == "rock" and choices[0] == "paper":
-    lose = 1
-else:
-    lose = 0
-if lose == 1:
-    print(f"Sorry, but the computer chose {choices[0]}")
-elif lose == 0:
-    print(f"Well done. The computer chose {choices[0]} and failed")
-elif lose == -1:
-    print(f"There is a draw ({choices[0]})")
-else:
-    print("How?")
+
+correct = ["scissors", "paper", "rock", "!exit"]
+while True:
+    choices = ["scissors", "paper", "rock"]
+    ans = input()
+    random.shuffle(choices)
+    lose = None
+    if ans not in correct:
+        print("Invalid input")
+    elif ans == "!exit":
+        print("Bye!")
+        break
+    elif ans == choices[0]:
+        lose = -1
+    elif ans == "scissors" and choices[0] == "rock":
+        lose = 1
+    elif ans == "paper" and choices[0] == "scissors":
+        lose = 1
+    elif ans == "rock" and choices[0] == "paper":
+        lose = 1
+    else:
+        lose = 0
+    if lose is not None:
+        if lose == 1:
+            print(f"Sorry, but the computer chose {choices[0]}")
+        elif lose == 0:
+            print(f"Well done. The computer chose {choices[0]} and failed")
+        elif lose == -1:
+            print(f"There is a draw ({choices[0]})")
+        else:
+            print("How?")
