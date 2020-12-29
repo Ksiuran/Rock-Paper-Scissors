68761df8057caec939c1dda4812d509dea696d47
Wilson Neely <naruisk.art@gmail.com>
2020-12-28 19:48:11 -0600

    Initial commit, stage 1 complete

---
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -0,0 +1,10 @@
+# Stage 1, used to break the ice and have somewhere to start
+ans = input()
+if ans == "scissors":
+    print("Sorry, but the computer chose rock")
+elif ans == "paper":
+    print("Sorry, but the computer chose scissors")
+elif ans == "rock":
+    print("Sorry, but the computer chose paper")
+else:
+    pass
