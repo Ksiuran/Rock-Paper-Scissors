22fd5ae4994175bfe39b92ac1de8d34f0cc5090b
Wilson Neely <naruisk.art@gmail.com>
2021-03-25 16:55:49 -0500

    Stage 4 complete, added a rating system that saves to a txt file. Saves & loads your score from said file, uses your name as a uuid.

--- Rock-Paper-Scissors/task/rps/game.py
+++ Rock-Paper-Scissors/task/rps/game.py
@@ -1,8 +1,38 @@
-# Stage 1, used to break the ice and have somewhere to start
 import random
 
 
-correct = ["scissors", "paper", "rock", "!exit"]
+def save(name, score):
+    ratings = open('rating.txt', "a", encoding='utf-8')
+    ratings.write(name + " " + str(score) + "\n")
+    ratings.close()
+    return
+
+
+def load(name):
+    f = False
+    lname = " "
+    lscore = " "
+    ratings = open('rating.txt', "r", encoding='utf-8')
+    for line in ratings:
+        lname, lscore = line.split(" ")
+        if lname == name:
+            f = True
+            break
+    ratings.close()
+    if f:
+        return int(lscore)
+    else:
+        return 0
+
+
+# To create the file if it doesn't exist
+ratings = open('rating.txt', "a", encoding='utf-8')
+ratings.close()
+#
+name = input("Enter your name:")
+print(f"Hello, {name}")
+score = load(name)
+correct = ["scissors", "paper", "rock", "!exit", "!rating"]
 while True:
     choices = ["scissors", "paper", "rock"]
     ans = input()
@@ -13,6 +43,8 @@ while True:
     elif ans == "!exit":
         print("Bye!")
         break
+    elif ans == "!rating":
+        print(f"Your rating: {score}")
     elif ans == choices[0]:
         lose = -1
     elif ans == "scissors" and choices[0] == "rock":
@@ -28,7 +60,10 @@ while True:
             print(f"Sorry, but the computer chose {choices[0]}")
         elif lose == 0:
             print(f"Well done. The computer chose {choices[0]} and failed")
+            score += 100
         elif lose == -1:
             print(f"There is a draw ({choices[0]})")
+            score += 50
         else:
             print("How?")
+    save(name, score)
